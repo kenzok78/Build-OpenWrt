1a44104387ed7dc2e810fa7b82e2fdf287dfc2c5
kenzok8 <kenzok8@gmail.com>
2021-03-10 14:01:53 +0800

    update config

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,10 +11,9 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git litte https://github.com/kenzok8/litte' feeds.conf.default
