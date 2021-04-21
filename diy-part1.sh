eb1acffa30080d10e1fafeb1af17393c78f2479c
kenzok8 <kenzok8@gmail.com>
2021-04-21 19:37:26 +0800

    int

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,9 +11,8 @@
 #
 
 # Uncomment a feed source
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git litte https://github.com/kenzok8/litte' feeds.conf.default
