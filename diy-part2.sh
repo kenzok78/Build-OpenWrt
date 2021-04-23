093d1bb87b316ed35ffc362c4299bea25a9d5245
kenzok8 <kenzok8@gmail.com>
2021-04-23 21:52:34 +0800

    int

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,14 @@
 sed -i 's/192.168.1.1/192.168.3.251/g' package/base-files/files/bin/config_generate
 
 # 修改固件生成名字,增加当天日期
-sed -i 's/$(VERSION) &&/$(VERSION) ;/g' include/download.mk
-date=`date +%m.%d`
-sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V %C by kenzo'/g" package/base-files/files/etc/openwrt_release
-sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mk
+sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release
+echo "DISTRIB_REVISION='small'" >> /etc/openwrt_release
+sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='openwrt '" >> /etc/openwrt_release
+
+sed -i 's/LuCI Master/kenzo/g' /usr/lib/lua/luci/version.lua
+sed -i 's/LuCI openwrt-18.06 branch/kenzo/g' /usr/lib/lua/luci/version.lua
+sed -i 's/LuCI 17.01 Lienol/kenzo/g' /usr/lib/lua/luci/version.lua
+sed -i '/luciversion/d' /usr/lib/lua/luci/version.lua
+echo 'luciversion = "Apr"' >> /usr/lib/lua/luci/version.lua
+
