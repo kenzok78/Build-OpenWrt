8f69b7db1c4c08b34da24fc0d3ddf05207071e67
kenzok8 <kenzok8@gmail.com>
2021-04-23 15:42:11 +0800

    int

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,6 +15,6 @@ sed -i 's/192.168.1.1/192.168.3.251/g' package/base-files/files/bin/config_gener
 
 # 修改固件生成名字,增加当天日期
 sed -i 's/$(VERSION) &&/$(VERSION) ;/g' include/download.mk
-date=`date +%m.%d.%Y`
+date=`date +%m.%d`
 sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V %C by kenzo'/g" package/base-files/files/etc/openwrt_release
 sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mk
