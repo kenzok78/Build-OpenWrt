a1d4d2238ce4ec6c499b596198479bdc6812bef4
kenzok8 <kenzok8@gmail.com>
2021-04-26 00:41:07 +0800

    int

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,13 +14,7 @@
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 
 # 修改版本号
-sed -i 's/kenzo/V${{ env.DATE }}/g' package/base-files/files/etc/banner
-
-# 自定义固件
-rm -rf package/default-settings/files/zzz-default-settings
-cp -f ../zzz-default-settings package/*/*/default-settings/files/
-
-# 修改版本号
-cid=$(date "+%Y-%m-%d")
-sed -i 's/kenzo/R[${cid}]/g' package/*/*/default-settings/files/zzz-default-settings
-
+sed -i 's/$(VERSION) &&/$(VERSION) ;/g' include/download.mk
+date=`date +%m.%d`
+sed -i 's/DISTRIB_DESCRIPTION/%D %V %C by kenzo/g' package/base-files/files/etc/openwrt_release
+sed -i 's/x/REVISION/$date/g' include/version.mk
