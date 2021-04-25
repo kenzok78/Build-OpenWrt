e942f184765749fad2b46fc7f0411daafb5e502c
wqzxhpx <32302128+wqzxhpx@users.noreply.github.com>
2021-04-25 21:08:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 # 修改固件生成名字,添加编译日期
 sed -i 's/$(VERSION) &&/$(VERSION) ;/g' include/download.mk
