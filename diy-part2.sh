ce7f15a98cb6d221a107bf02ed12c3d28e345bb2
kenzok8 <kenzok8@gmail.com>
2021-04-27 01:04:52 +0800

    int

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,6 +16,6 @@ sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generat
 # 修改版本号
 sed -i 's/$(VERSION) &&/$(VERSION) ;/g' include/download.mk
 date=`date +%m.%d`
-sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V %C by kenzo'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V by kenzo'/g" package/base-files/files/etc/openwrt_release
 sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mk
 cp -f feeds/litte/banner package/base-files/files/etc/banner
