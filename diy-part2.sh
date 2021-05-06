9c268587de70e6066020a2ebfaaa44e1977bb8f5
kenzok8 <kenzok8@gmail.com>
2021-05-06 19:03:11 +0800

    int

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,3 +24,14 @@ date=`date +%m.%d`
 sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V by kenzo'/g" package/base-files/files/etc/openwrt_release
 sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mk
 cp -f feeds/litte/commit/banner package/base-files/files/etc/banner
+cp -f feeds/litte/commit/luci-mod-admin-full feeds/luci/modules/luci-mod-admin-full/root/etc/uci-defaults/50_luci-mod-admin-full
+cp -f feeds/litte/commit/index_x86.htm feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index_x86.htm
+cp -f feeds/litte/commit/index_x86.htm feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+
+# 替换默认主题
+sed -i 's#luci-theme-bootstrap#luci-theme-argon#g' feeds/luci/collections/luci/Makefile
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+# cpufreq
+sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
+sed -i 's/services/system/g' package/lean/luci-app-cpufreq/luasrc/controller/cpufreq.lua
