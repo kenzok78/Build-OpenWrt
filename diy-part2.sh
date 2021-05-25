a46a05f48181e4010fa3e010b45e5374b5e773e0
kenzok8 <kenzok8@gmail.com>
2021-05-25 17:04:18 +0800

    int

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,5 +23,5 @@ sed -i 's/$(VERSION) &&/$(VERSION) ;/g' include/download.mk
 date=`date +%m.%d`
 sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V by kenzo'/g" package/base-files/files/etc/openwrt_release
 sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mk
-cp -f feeds/litte/commit/banner package/base-files/files/etc/banner
+
 
