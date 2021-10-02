bab77e5d8d30eebe120167d622b33080278cafa8
有種 <39034242+kenzok8@users.noreply.github.com>
2021-10-02 16:20:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-
-# Modify default IP
+# --------------------------------------------------------
+# Script to compile and create files for each openwrt
+# --------------------------------------------------------
+#1. Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 
-
-# bypass 编译
-#find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
-#find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
-
-# 修改版本号
-sed -i 's/$(VERSION) &&/$(VERSION) ;/g' include/download.mk
 date=`date +%m.%d`
-sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V by kenzo'/g" package/base-files/files/etc/openwrt_release
-sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mk
+date1=`date +%h`
+sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION=\"0penwrt'\"/g" package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_REVISION.*/DISTRIB_REVISION=\"$date'\"/g" package/base-files/files/etc/openwrt_release
+sed -i "s/%C.*/%C=\"$date1'\"/g" package/base-files/files/etc/openwrt_version
+sed -i "s/ %D %V, %C.*/ %D %V, %C=\"$date1 by kenzo'\"/g"  package/base-files/files/etc/banner
+#Lean设置
+sed -i "s/LuCI Master.*/LuCI Master=\"kenzo'\"/g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/luciversion.*/luciversion=\"$date1'\"/g" package/lean/default-settings/files/zzz-default-settings
+#ctc设置
+sed -i "s/LuCI openwrt-18.06 branch.*LuCI openwrt-18.06 branch=\"kenzo'\"/g" package/emortal/default-settings/files/zzz-default-settings
+sed -i "s/LuCI openwrt-21.02 branch.*LuCI openwrt-21.02 branch=\"kenzo'\"/g" package/emortal/default-settings/files/zzz-default-settings
+sed -i "s/luciversion.*luciversion=\"$date1'\"/g" package/emortal/default-settings/files/zzz-default-settings
+#Lienol设置
+sed -i "s/17.01 Lienol.*/17.01 Lienol=\"kenzo'\"/g" package/default-settings/files/zzz-default-settings
+sed -i "s/luciversion.*/luciversion=\"$date1'\"/g" package/default-settings/files/zzz-default-settings
+
+# 修改自定义固件名,增加编译日期(by:kenzo）
+sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=$date-$(VERSION_DIST_SANITIZED)/g' include/image.mk
+
+
+exit
+
 
 
