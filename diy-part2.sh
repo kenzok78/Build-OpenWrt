3f20ee5996a6a7ab5daa2f093133b505ce82b13a
有種 <39034242+kenzok8@users.noreply.github.com>
2021-10-03 16:55:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,16 +11,6 @@ sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION=\"0penwrt'\"/g" package/base
 sed -i "s/DISTRIB_REVISION.*/DISTRIB_REVISION=\"$date'\"/g" package/base-files/files/etc/openwrt_release
 sed -i "s/%C.*/%C=\"$date1'\"/g" package/base-files/files/etc/openwrt_version
 sed -i "s/ %D %V, %C.*/ %D %V, %C=\"$date1 by kenzo'\"/g"  package/base-files/files/etc/banner
-#Lean设置
-sed -i "s/LuCI Master.*/LuCI Master=\"kenzo'\"/g" package/lean/default-settings/files/zzz-default-settings
-sed -i "s/luciversion.*/luciversion=\"$date1'\"/g" package/lean/default-settings/files/zzz-default-settings
-#ctc设置
-sed -i "s/LuCI openwrt-18.06 branch.*LuCI openwrt-18.06 branch=\"kenzo'\"/g" package/emortal/default-settings/files/zzz-default-settings
-sed -i "s/LuCI openwrt-21.02 branch.*LuCI openwrt-21.02 branch=\"kenzo'\"/g" package/emortal/default-settings/files/zzz-default-settings
-sed -i "s/luciversion.*luciversion=\"$date1'\"/g" package/emortal/default-settings/files/zzz-default-settings
-#Lienol设置
-sed -i "s/17.01 Lienol.*/17.01 Lienol=\"kenzo'\"/g" package/default-settings/files/zzz-default-settings
-sed -i "s/luciversion.*/luciversion=\"$date1'\"/g" package/default-settings/files/zzz-default-settings
 
 exit
 
