71625f1747e47046802f1d975989fe70f9c64841
有種 <39034242+kenzok8@users.noreply.github.com>
2021-10-04 17:31:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,16 +3,8 @@
 # Script to compile and create files for each openwrt
 # --------------------------------------------------------
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.252/g' package/base-files/files/bin/config_generate
 
-date=`date +%m.%d`
-date1=`date +%h`
-sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION=\"0penwrt'\"/g" package/base-files/files/etc/openwrt_release
-sed -i "s/DISTRIB_REVISION.*/DISTRIB_REVISION=\"$date'\"/g" package/base-files/files/etc/openwrt_release
-sed -i "s/%C.*/%C=\"$date1'\"/g" package/base-files/files/etc/openwrt_version
-sed -i "s/ %D %V, %C.*/ %D %V, %C=\"$date1 by kenzo'\"/g"  package/base-files/files/etc/banner
-
-exit
 
 
 
