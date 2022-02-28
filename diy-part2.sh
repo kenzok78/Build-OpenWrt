8151ccec156b409730636773140259ea41c6abe0
有種 <39034242+kenzok8@users.noreply.github.com>
2022-02-28 16:10:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,16 +4,8 @@
 # --------------------------------------------------------
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.1.252/g' package/base-files/files/bin/config_generate
-sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += lm-sensors-detect kmod-r8125 kmod-vmxnet3  kmod-igc/' target/linux/x86/Makefile
 
-echo '
-CONFIG_CRYPTO_CHACHA20_X86_64=y
-CONFIG_CRYPTO_POLY1305_X86_64=y
-CONFIG_DRM=y
-CONFIG_DRM_I915=y
-' >> ./target/linux/x86/config-5.10
 
-sed -i "s/enabled '0'/enabled '1'/g" feeds/packages/utils/irqbalance/files/irqbalance.config
 
 
 
